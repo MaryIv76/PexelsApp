5e8df678ee221ce21077478287793c8efc1164cf
MaryIv76 <ivanova_mary02@mail.ru>
2023-09-17 23:43:12 +0300

    Add bottom navigation

--- app/src/main/java/com/ivanova/pexelsapp/MainActivity.kt
+++ app/src/main/java/com/ivanova/pexelsapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.ivanova.pexelsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.itemIconTintList = null
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
