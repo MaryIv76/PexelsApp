bb86fab6c183a7887ae226278bd82c631a8a5bfd
MaryIv76 <ivanova_mary02@mail.ru>
2023-09-18 02:32:05 +0300

    Add search view

--- app/src/main/java/com/ivanova/pexelsapp/MainActivity.kt
+++ app/src/main/java/com/ivanova/pexelsapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ivanova.pexelsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView.itemIconTintList = null
         val navController = findNavController(R.id.fragmentContainerView)
-
         bottomNavigationView.setupWithNavController(navController)
+
     }
 }
